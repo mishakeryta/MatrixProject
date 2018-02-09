428570a8db4edf9d073cbf1cc63bafa96807144f
Misha <keryta.mi@gmail.com>
2018-02-09 19:59:13 +0200

    Add MatrixExeption.cs.Add to Matrix:double array elements,constructors,indexers,propertys Rows and Cols.

---
+++ MatrixProject/Matrix.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//It is matrix class
+namespace MatrixProject
+{
+    class Matrix
+    {
+        private double[,] element = null;
+        private int Rows { get; protected set; }
+        private int Cols { get; protected set; }
+        public double this[int row, int col]
+        {
+            //i dont know ,if i skeep this try and catch there will it be awalible to program catch  
+            get
+            {
+                try
+                {
+                    return this.element[row, col];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+            set
+            {
+                try
+                {
+                    this.element[row, col] = value;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw;
+                }
+            }
+        }
+        public Matrix(int rows = 1, int cols = 1)
+        {
+            try
+            {
+                this.element = new double[rows, cols];
+            }
+            catch (Exception exceptionObject)
+            {
+                //i dont know name of exceptions Need to be fixed.
+                Console.WriteLine(exceptionObject.Message);
+            }
+            this.Rows = rows;
+            this.Cols = cols;
+        }
+        public Matrix(int rows, int cols, params double[] elements)
+        {
+            if (rows == 0 || cols == 0)
+            {
+                throw new ArgumentException("Matrix cant have 0 rows or(and) cols.");
+            }
+            try
+            {
+                this.element = new double[rows, cols];
+                for (int i = 0; i < elements.Length;++i)
+                {
+                    this.element[i / cols, i % cols] = elements[i];
+                }
+            }
+            catch(OutOfMemoryException)
+            {
+                throw;
+            }
+            catch
+            {
+                throw;
+            }
+            this.Rows = rows;
+            this.Cols = cols;
+        }
+        public Matrix(Matrix matrix)
+        {
+
+            this.element = new double[matrix.Rows, matrix.Cols];
+            for (int i = 0; i < matrix.Rows; ++i)
+            {
+                for (int j = 0; j < matrix.Cols; ++j)
+                {
+                    this.element[i, j] = matrix[i, j];
+                }
+            }
+            this.Rows = rows;
+            this.Cols = cols;
+        }
+        public override string ToString()
+        {
+            string strMatrix = "";
+            for(int i = 0;i<this.Rows;++i)
+            {
+                for(int j = 0;j<this.Cols;++j)
+                {
+                    strMatrix += this.element[i, j].ToString + " ";
+                }
+                strMatrix += "\n";
+            }
+            return strMatrix;
+        }
+
+    }
+}
